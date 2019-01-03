6ae99fb75bacca448d4d2d9034b270bf54002049
Przemysław Kaleta <kaletap@student.mini.pw.edu.pl>
2019-01-03 22:38:39 +0100

    error handling in get_location function

--- location.py
+++ location.py
@@ -4,52 +4,64 @@ import requests
 from utils import load_scifi_table
 
 
+def return_na(get_country, get_city):
+    if get_country:
+        if get_city:
+            return tuple(np.nan for _ in range(4))
+        else:
+            return tuple(np.nan for _ in range(3))
+    else:     
+        return np.nan, np.nan
+
+
 def get_location_data(location, get_country=False, get_city=False):
+    print(location)
     """Returns location data (geographical coordinates and country and city)
     * lattitude, longitude, country, city if get_country and get_city are True
     * lattitude, longitude, country if only get_country is True
     * lattitude, longitude otherwise
     """
     if location is np.nan:
-        if get_country:
-            if get_city:
-                return tuple(np.nan for _ in range(4))
-            else:
-                return tuple(np.nan for _ in range(3))
-        else:     
+        return return_na(get_country, get_city)
+    try:
+        address_splitted = location.split(" ")
+        address_splitted = [x.strip() for x in address_splitted]
+        address = "+".join(address_splitted)
+        url = ("http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false"
+            "&address={}").format(address)
+
+        r = requests.get(url)
+        result = r.json()['results']
+        if not result:
             return np.nan, np.nan
 
-    address_splitted = location.split(" ")
-    address_splitted = [x.strip() for x in address_splitted]
-    address = "+".join(address_splitted)
-    url = ("http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false"
-           "&address={}").format(address)
+        coordinates = result[0]["geometry"]["location"]
 
-    r = requests.get(url)
-    result = r.json()['results']
-    if not result:
-        return np.nan, np.nan
+        country, city = np.nan, np.nan
+        if get_country or get_city:
+            address_components = result[0]['address_components']
+            for component in address_components:
+                if 'country' in component['types']: 
+                    country = component['long_name']
+                    # Unfortunately, sometimes country can be empty string, 
+                    # for example for location == "Europe"
+                elif 'locality' in component['types']:
+                    city = component['short_name']
 
-    coordinates = result[0]["geometry"]["location"]
+        if get_country:
+            if get_city:
+                return coordinates['lat'], coordinates['lng'], country, city
+            else:
+                return coordinates['lat'], coordinates['lng'], country
+        else:     
+            return coordinates['lat'], coordinates['lng']
 
-    country, city = np.nan, np.nan
-    if get_country or get_city:
-        address_components = result[0]['address_components']
-        for component in address_components:
-            if 'country' in component['types']: 
-                country = component['long_name']
-                # Unfortunately, sometimes country can be empty string, 
-                # for example for location == "Europe"
-            elif 'locality' in component['types']:
-                city = component['short_name']
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
 
-    if get_country:
-        if get_city:
-            return coordinates['lat'], coordinates['lng'], country, city
-        else:
-            return coordinates['lat'], coordinates['lng'], country
-    else:     
-        return coordinates['lat'], coordinates['lng']
+    except Exception as exc:
+        print("!!!Exception of type {} on location {}".format(str(exc), location))
+        return_na(get_country, get_city)
 
 
 def get_geocode(location):
